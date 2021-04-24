cd440dade17f71b9b66861500b1bb8195c4751d4
Sara <48062125+srahkmli@users.noreply.github.com>
2021-04-24 12:56:07 +0430

    Event Modifier

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      yourName: '',
+      confirmedInput: '',
     };
   },
   methods: {
+    confirmedName() {
+      this.confirmedInput = this.yourName;
+    },
+    submitForm() {
+      alert('submitted!');
+    },
+    setName(event, lastName) {
+      this.yourName = event.target.value + ' ' + lastName;
+    },
     add() {
       this.counter = this.counter + 1;
     },
