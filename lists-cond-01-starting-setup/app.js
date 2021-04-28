fb7d069646c5e9ea679efde70a84aabf68abc1a6
Sara <48062125+srahkmli@users.noreply.github.com>
2021-04-29 02:11:43 +0430

    conditional rendering

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,7 +1,24 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      enteredGaol: '',
+      goals: [],
+      person: { name: 'sara', age: 20, field: 'programmer' }
+
+    };
   },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGaol);
+      this.enteredGaol = '';
+    },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
+    }
+  }
 });
 
 app.mount('#user-goals');
+if (true) {
+  // ...
+}
