3887f0effdf91077fa70d3d432b19caa0a6d1529
Sara <48062125+srahkmli@users.noreply.github.com>
2021-05-04 00:40:10 +0430

    styled first ap[

---
+++ vue-cli-project/src/components/FriendsContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "101",
+        name: "sara",
+        phone: "9891222222",
+        email: "srahkmli",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
--- vue-cli-project/src/main.js
+++ vue-cli-project/src/main.js
@@ -1,3 +1,11 @@
 import { createApp } from 'vue';
 
-createApp({}).mount('');
+import App from './App.vue';
+import FriendContact from './components/FriendsContact.vue';
+
+
+const app = createApp(App);
+
+app.component('friend-contact', FriendContact);
+
+app.mount('#app');
