9bd6a335e68c5e781abc01ad2a099e104b7f7eb1
Sara <48062125+srahkmli@users.noreply.github.com>
2021-05-07 17:01:23 +0430

    add friend added

--- cmp-communication/src/App.vue
+++ cmp-communication/src/App.vue
@@ -4,6 +4,8 @@
       <h1>My Friends</h1>
     </header>
     <ul>
+      <new-friend @add-contact="addContact"> </new-friend>
+
       <friend-contact
         v-for="friend in friends"
         :key="friend.id"
@@ -13,6 +15,7 @@
         :email-address="friend.email"
         :is-favourite="friend.isFavourite"
         @toggle-faveorite="toggleFaveStatus"
+        @delete="deleteContact"
       ></friend-contact>
     </ul>
   </section>
@@ -28,24 +31,37 @@ export default {
           name: "Manuel Lorenz",
           phone: "0123 45678 90",
           email: "manuel@localhost.com",
-          isFavourite:false,
+          isFavourite: false,
         },
         {
           id: "julie",
           name: "Julie Jones",
           phone: "0987 654421 21",
           email: "julie@localhost.com",
-          isFavourite:false,
+          isFavourite: false,
         },
       ],
     };
-  },//data end
-   methods:{
-     toggleFaveStatus(friendId){
-      const identifier = this.friends.find((friend)=>friend.id===friendId);
-      identifier.isFavourite =!identifier.isFavourite;
-     },
-   }
+  }, //data end
+  methods: {
+    toggleFaveStatus(friendId) {
+      const identifier = this.friends.find((friend) => friend.id === friendId);
+      identifier.isFavourite = !identifier.isFavourite;
+    },
+    addContact(name, phone , email ){
+      const newFriendContact={
+       id: new Date().toISOString(),
+          name: name,
+          phone: phone,
+          email: email,
+          isFavourite: false,
+          };
+          this.friends.push(newFriendContact);
+    },
+    deleteContact(friendId){
+      this.friends=this.friends.filter((contact)=> contact.id !== friendId);
+    },
+  },
 };
 </script>
 
@@ -75,7 +91,8 @@ header {
   padding: 0;
   list-style: none;
 }
-#app li {
+#app li ,
+#app form{
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -104,5 +121,18 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
---
+++ cmp-communication/src/components/newFriend.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label>email</label>
+      <input type="email" v-model="enteredEmail" />
+    </div>
+    <div>
+      <label>phone</label>
+      <input type="tel" v-model="enteredPhone" />
+    </div>
+    <div>
+      <button>add friend</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-contact"],
+  data() {
+    return {
+      enteredName: "",
+      enteredEmail: "",
+      enteredPhone: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit(
+        "add-contact",
+        this.enteredName,
+        this.enteredEmail,
+        this.enteredPhone
+      );
+    },
+  },
+};
+</script>
